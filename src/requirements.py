4d1e0be2a94606a8b4af9fdb7a5c8b04da1604ea
Eugene Tan <jmingtan@gmail.com>
2008-08-18 00:30:24 +0800

    Change log output paths to be more consistent with tpclient-pywx

--- src/requirements.py
+++ src/requirements.py
@@ -1,7 +1,54 @@
 # This file checks you have installed the requirements for tpclient-pyogre 
 # It can be run as standalone but is also run by the client at startup
 
+import sys
+import os.path
+
 notfound = []
+
+def configpath():
+	"""\
+	Figures out where to save the preferences.
+	"""
+	dirs = [("APPDATA", "Thousand Parsec"), ("HOME", ".tp"), (".", "var")]
+	for base, extra in dirs:
+		if base in os.environ:
+			base = os.environ[base]
+		elif base != ".":
+			continue
+			
+		rc = os.path.join(base, extra)
+		if not os.path.exists(rc):
+			os.mkdir(rc)
+		return rc
+
+class FileOutput(object):
+	def __init__(self, stdout=None):
+		self.o = stdout
+
+		self.f = open(os.path.join(configpath(), "tpclient-pyogre.log"), "wb")
+		if not self.o is None:
+			self.o.write("Output going to %s\n" % self.f.name)
+
+	def write(self, s):
+		if isinstance(s, unicode):
+			s = s.encode('utf-8')
+
+		if not self.o is None:
+			self.o.write(s)
+		self.f.write(s)
+	
+	def __del__(self):
+		if not self.o is None:
+			import os
+			os.unlink(self.f.name)
+
+if hasattr(sys, "frozen"):
+	sys.stdout = FileOutput()
+	sys.stderr = sys.stdout
+else:
+	sys.stdout = FileOutput(sys.stdout)
+	sys.stderr = sys.stdout
 
 try:
 	import ogre.renderer.OGRE as ogre
