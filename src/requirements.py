a14029da1a59d56d28f1a5c6863d3269d3bef066
Eugene Tan <jmingtan@gmail.com>
2008-08-20 01:08:01 +0800

    Update requirements to do some version checking and to check for ogreal

--- src/requirements.py
+++ src/requirements.py
@@ -5,6 +5,7 @@ import sys
 import os.path
 
 notfound = []
+recommended = []
 
 def configpath():
 	"""\
@@ -47,10 +48,32 @@ if hasattr(sys, "frozen"):
 	sys.stdout = FileOutput()
 	sys.stderr = sys.stdout
 
+def tostr(ver1):
+	s = ""
+	for a in ver1:
+		s += "."+str(a)
+	return s[1:]
+
+print "Client information:"
+print "---------------------------------------------------------------"
+import version
+#try:
+	#print "Client version", version.version_str+'+'+version.version_target_str, "(git %s)" % version.version_git
+#except AttributeError:
+print "Client version", version.version_str
+print "Running from ", os.path.dirname(os.path.join(os.path.abspath(__file__)))
+print
+
+print "Checking requirements:"
+print "---------------------------------------------------------------"
+
+ogre_version = "1.4"
 try:
 	import ogre.renderer.OGRE as ogre
+	if ogre.ogre_version != ogre_version:
+		notfound.append("python-ogre/ogre == " + ogre_version)
 except ImportError:
-	notfound.append("python-ogre")
+	notfound.append("python-ogre/ogre")
 
 try:
 	import ogre.gui.CEGUI as cegui
@@ -58,20 +81,55 @@ except ImportError:
 	notfound.append("python-ogre/cegui")
 
 try:
-	import tp.netlib
+	import ogre.sound.OgreAL as ogreal
 except ImportError:
-	import sys
-	sys.path.append("..")
-	
-	try:
-		import tp.netlib
-	except ImportError:
-		notfound.append("tp.netlib")
+	reason = "The OgreAL addon, which is required for audio, does not seem to be installed"
+	recommended.append(("python-ogre/ogreal", reason))
 
+netlib_version = (0, 2, 4)
+try:
+	import tp.netlib
+
+	print "Thousand Parsec Protocol Library Version", tp.netlib.__version__ 
+	try:
+		print "    (installed at %s)" % tp.netlib.__installpath__
+	except AttributeError:
+		print "    (version too old to work out install path)"
+
+	try:
+		from tp.netlib.version import version_git
+		print "    (git %s)" % version_git
+	except ImportError:
+		print
+
+	if cmp(netlib_version, tp.netlib.__version__) < 0:
+		raise ImportError("Thousand Parsec Network Library (libtpproto-py) is too old")
+
+except (ImportError, KeyError, AttributeError), e:
+	print e
+	notfound.append("tp.netlib >= " + tostr(netlib_version))
+
+client_version = (0, 3, 1)
 try:
 	import tp.client
-except ImportError:
-	notfound.append("tp.client")
+
+	print "Thousand Parsec Client Library Version", tp.client.__version__
+	try:
+		print "    (installed at %s)" % tp.client.__installpath__
+	except AttributeError:
+		print "    (version too old to work out install path)"
+	
+	try:
+		from tp.client.version import version_git
+		print "    (git %s)" % version_git
+	except ImportError:
+		print
+
+	if cmp(client_version, tp.client.__version__) < 0:
+		raise ImportError("Thousand Parsec Client Library (libtpclient-py) is too old")
+except (ImportError, KeyError, AttributeError), e:
+	print e
+	notfound.append("tp.client >= " + tostr(client_version))
 
 import __builtin__
 try:
@@ -93,6 +151,28 @@ except Exception, e:
 	print e
 	notfound.append("local config files")
 
+	print
+	print "Checking locations:"
+	print "---------------------------------------------------------------"
+	import os
+	try:
+		graphicsdir = os.environ["TPCLIENT_MEDIA"]
+	except KeyError:
+		graphicsdir = '../media'
+	print "Media files are in %s" % graphicsdir
+	if not os.path.exists(os.path.join(graphicsdir, 'icons/icon.ico')):
+		print "Can not find media required by this client."
+		sys.exit()
+
+	try:
+		windir = os.environ["TPCLIENT_WINDOW"]
+	except KeyError:
+		windir = 'doc'
+	print "Window layouts are in %s" % windir
+	if not os.path.exists(os.path.join(windir, 'system.layout')):
+		print "Can not find window layouts required by this client."
+		sys.exit()
+
 if len(notfound) > 0:
 	print "The following requirements where not met"
 	for module in notfound:
@@ -100,3 +180,31 @@ if len(notfound) > 0:
 
 	import sys
 	sys.exit(1)
+
+import os, pprint
+try:
+	COLS = int(os.environ["COLUMNS"])
+except (KeyError, ValueError):
+	try:
+		import struct, fcntl, sys, termios
+		COLS = struct.unpack('hh', fcntl.ioctl(sys.stdout, termios.TIOCGWINSZ, '1234'))[1]
+	except:
+		COLS = 80
+
+ALIGN = 25
+if len(recommended) > 0:
+	print "The following recommended modules where not found:"
+	for module, reason in recommended:
+		lines = [""]
+		lines[-1] += '    %s, ' % module
+		lines[-1] += ' ' * (ALIGN-len(lines[-1]))
+
+		for word in reason.split(" "):
+			if (len(lines[-1]) + len(word) + 2) > COLS:
+				lines.append(' '*ALIGN)
+
+			lines[-1] += word + " "
+
+		print
+		print "\n".join(lines)
+
