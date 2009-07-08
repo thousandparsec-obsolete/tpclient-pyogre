e80b8d31c69f6d8c26f5f2120dcfc5cab5e2f5e4
Landon Fowles <landon@lfowles.org>
2009-07-07 23:56:50 -0500

    Fixed window fading issues, now all children have the event added

--- src/battleviewer.py
+++ src/battleviewer.py
@@ -91,6 +91,7 @@ class GUIFadeListener(ogre.FrameListener):
 	def __init__(self):
 		ogre.FrameListener.__init__(self)
 		self.elements = {}
+		self.child_map = {}
 		self.wm = cegui.WindowManager.getSingleton()
 
 	def registerElement(self, element, min_alpha=0.01, fadeout_time=1.0, fadein_time=1.0):
@@ -106,12 +107,27 @@ class GUIFadeListener(ogre.FrameListener):
 							'event':True}
 		helpers.bindEvent(element, self, "show", cegui.Window.EventMouseEnters)
 		helpers.bindEvent(element, self, "hide", cegui.Window.EventMouseLeaves)
+		self.bindChildren(element, element)
+
+	def bindChildren(self, name, base):
+		window = self.wm.getWindow(name)
+		num_children = window.getChildCount()
+		for i in range(num_children):
+			child = window.getChildAtIdx(i)
+			child_name = child.getName().c_str()
+			self.child_map[child_name] = base
+			helpers.bindEvent(child_name, self, "show", cegui.Window.EventMouseEnters)
+			helpers.bindEvent(child_name, self, "hide", cegui.Window.EventMouseLeaves)
+			self.bindChildren(child_name, base)
+
 
 	def show(self, evt, event_call=True):
 		if isinstance(evt, str):
 			element = evt
 		else:
 			element = evt.window.getName().c_str()
+		if element in self.child_map:
+			element = self.child_map[element]
 		self.elements[element]['active'] = True
 		self.elements[element]['event'] = event_call
 		self.elements[element]['direction'] = 'in'
@@ -121,6 +137,8 @@ class GUIFadeListener(ogre.FrameListener):
 			element = evt
 		else:
 			element = evt.window.getName().c_str()
+		if element in self.child_map:
+			element = self.child_map[element]
 		self.elements[element]['active'] = True
 		self.elements[element]['event'] = event_call
 		self.elements[element]['direction'] = 'out'
